d4b433c1e931134cf4fe1d1a6f31f41c365e5e9b
Ryan Graff <ryan@StellarOmega.local>
2017-07-11 19:47:17 -0400

    feat: Add the basis for AbstractTypePersistenceDriver.

--- src/AbstractTypePersistenceDriver.jsx
+++ src/AbstractTypePersistenceDriver.jsx
@@ -1,5 +1,41 @@
+/**
+ * An abstract class representing the API of a Type Persistence Driver
+ * implementation.
+ * @param {Object} config An object with properties to be assigned to the new
+ * instance.
+ * */
 export default class AbstractTypePersistenceDriver {
   constructor (config) {
     Object.assign(this, config);
+  }
+
+  /**
+   * Create an item of the given type.
+   * @abstract
+   * @param {Object} item The item to be created.
+   * @param {string} typeName The name of the type.
+   * @returns {string} The id of the newly created item.
+   * */
+  async create (item, typeName) {
+  }
+
+  /**
+   * Read an item of the given type by id.
+   * @abstract
+   * @param {string} id The id of the item to be read.
+   * @param {string} typeName The name of the type.
+   * @returns {Object} The item.
+   * */
+  async read (id, typeName) {
+  }
+
+  /**
+   * Update an item of the given type.
+   * @abstract
+   * @param {Object} item The item to be updated.
+   * @param {string} item.id The id of the item to be updated.
+   * @param {string} typeName The name of the type.
+   * */
+  async update (item, typeName) {
   }
 }
